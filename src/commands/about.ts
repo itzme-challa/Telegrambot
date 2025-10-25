196b019458c36b57b781c292407e7e41a60ce303
itzme-challa <challawaheed4@gmail.com>
2025-10-25 23:19:39 +0530

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,14 +1,19 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
-
-import { author, name, version } from '../../package.json';
+import packageJson from '../../package.json'; // Causing the error
 
 const debug = createDebug('bot:about_command');
 
 const about = () => async (ctx: Context) => {
-  const message = `*${name} ${version}*\n${author}`;
-  debug(`Triggered "about" command with message \n${message}`);
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+  debug('Triggered "about" command');
+  const messageId = ctx.message?.message_id;
+  const reply = `This is a Telegram bot to connect users randomly. Version: ${packageJson.version}\nUse /search to find a partner, /stop to end a dialog, /link to request a profile, and /share to share your profile.`;
+
+  if (messageId) {
+    await ctx.reply(reply, {
+      reply_parameters: { message_id: messageId },
+    });
+  }
 };
 
 export { about };
